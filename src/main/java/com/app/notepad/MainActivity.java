b80a6fa54072a5d44e54e3d0a072dd5b15f7ad1f
Pavel Vasilev <76531243+GyrateTiara8831@users.noreply.github.com>
2021-03-09 23:33:07 +0300

    Add files via upload

---
+++ src/main/java/com/app/notepad/MainActivity.java
@@ -0,0 +1,71 @@
+package com.app.notepad;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity{
+
+    private int id;
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private MenuItem menuSearch;
+    private MenuItem menuSort;
+    private MenuItem menuSave;
+    private MenuItem menuDelete;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_settings)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        menuSearch = menu.findItem(R.id.action_search);
+        menuSort = menu.findItem(R.id.action_sort);
+        menuDelete = menu.findItem(R.id.action_delete);
+        menuSave = menu.findItem(R.id.action_save);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void click_new(View view) {
+        Navigation.findNavController(view).navigate(R.id.nav_entery);
+    }
+}
---
+++ src/main/java/com/app/notepad/ui/Home/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.app.notepad.ui.Home;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.app.notepad.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        super(R.layout.fragment_home);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
+
+
